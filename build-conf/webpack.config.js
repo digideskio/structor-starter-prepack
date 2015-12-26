06bee3f0ddbf4d13b7907c1f2793777b9b657bd7
Alexander Pustovalov <apustovalov@gmail.com>
2015-12-26 11:15:16 +0200

    migrate for Structor version 0.2.4

--- build-conf/webpack.config.js
+++ build-conf/webpack.config.js
@@ -13,9 +13,24 @@ module.exports = [
         },
         module: {
             loaders: [
-                { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
-                { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
-                { test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)([\?]?.*)$/, exclude: /node_modules/, loader: 'url-loader' }
+                {
+                    test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel',
+                    query: {
+                        cacheDirectory: true,
+                        presets: ['react', 'es2015', 'stage-0'],
+                        plugins: ['transform-runtime', 'add-module-exports']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                },
+                {
+                    test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)([\?]?.*)$/,
+                    exclude: /node_modules/,
+                    loader: 'url-loader'
+                }
             ]
         },
         plugins: [
--- src/client/main.js
+++ src/client/main.js
@@ -1,4 +1,4 @@
-import 'babel-core/polyfill';
+import 'babel-polyfill';
 
 import './assets/css/font-awesome.css';
 
