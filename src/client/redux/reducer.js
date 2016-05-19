9069d00733df945fe92b712c544c7ea07ece7c9c
Alexander Pustovalov <apustovalov@gmail.com>
2016-05-20 00:24:44 +0300

    for Structor v1.0.7

--- src/client/redux/reducer.js
+++ src/client/redux/reducer.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux';
-import test1Reducer from '../controllers/Test/Test1/reducer.js';
-const reducer = combineReducers({ test1: test1Reducer });
+const reducer = combineReducers({
+});
 export default reducer;
--- src/client/redux/saga.js
+++ src/client/redux/saga.js
@@ -2,7 +2,5 @@ import {
     fork,
     take
 } from 'redux-saga/effects';
-import test1Saga from '../controllers/Test/Test1/sagas.js';
 export default function* mainSaga() {
-    yield fork(test1Saga);
 }
